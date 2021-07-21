7e20ba3808b84eaf400429ea506a05ceadf6244b
Joe Christensen <joechristensenwork@gmail.com>
2021-07-21 16:02:35 -0700

    commit tree 7d78c19eb6b0cbf3ec50d306729702feffa5cc55

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import SpicyFoodList from "./SpicyFoodList";
+import {Filter} from "./SpicyFoodList";
 
 function App() {
   return (
     <div>
       <h3>SpicyFoodList</h3>
+      <Filter />
       <SpicyFoodList />
     </div>
   );
--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -6,15 +6,64 @@ function SpicyFoodList() {
 
   function handleAddFood() {
     const newFood = getNewSpicyFood();
+    setFoods((foods) => [...foods, newFood])
     console.log(newFood);
+  }
+
+  function handleLiClick(id) {
+    // setFoods((foods) => foods.filter((element) => {
+    //   return element.id !== id;
+    // }))
+    setFoods((foods) => foods.map((food) => {
+      if(food.id === id) {
+        food.heatLevel++;
+        return food;
+      }
+      return food;
+    }))
   }
 
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
-      <ul>{/* list of spicy foods */}</ul>
+      <ul>{foods.map((element) => {
+        return (
+          <li onClick={() => {handleLiClick(element.id)}} 
+          key={element.id}>
+            {`${element.name} | Heat: ${element.heatLevel} | Cuisine: ${element.cuisine}`}
+            </li>
+        )
+      })}</ul>
     </div>
   );
 }
 
+function Filter() {
+  const [filterBy, setFilterBy] = useState("All");
+  const [foods, setFoods] = useState(spicyFoods);
+  function handleFilterChange(event) {
+    setFilterBy(event.target.value);
+  }
+
+  const foodsToDisplay = foods.filter((food) => {
+    if(filterBy ==="All") {
+      return true;
+    }
+    else {
+      return food.cuisine === filterBy;
+    }
+  });
+
+  return (
+    <select name="filter" onChange={handleFilterChange}>
+        <option value="All">All</option>
+        <option value="American">American</option>
+        <option value="Sichuan">Sichuan</option>
+        <option value="Thai">Thai</option>
+        <option value="Mexican">Mexican</option>
+      </select> 
+  );
+}
+
 export default SpicyFoodList;
+export {Filter};
